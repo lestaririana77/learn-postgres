ae6eec1539e49f1c8ce1cb03994b12e9fe36d182
lestaririanaa@gmail.com <lestaririana77>
2023-01-12 22:32:31 +0700

    create table, select & insert

---
+++ 01-CreateTable.sql
@@ -0,0 +1,11 @@
+create table list_data(
+	id_user char(4) primary key,
+	nama_user varchar(100) not null
+);
+
+create table mytable (
+	somevalue VARCHAR (20)
+);
+
+insert into mytable (somevalue)
+values ('Hello');
